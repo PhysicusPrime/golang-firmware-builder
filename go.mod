33c4cca798c6c2e17b15feea0ad8303c853811b8
Your Name <you@example.com>
2025-08-27 05:56:45 +0200

    ADDED

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/PhysicusPrime/golang-firmware-builder
 
-go 1.25
+go 1.19
+require (
+    github.com/PhysicusPrime/golang-firmware-builder v0.0.0-20240606023456-4f3b6f3e1c2d
+    github.com/PhysicusPrime/golang-firmware-builder/busybox v0.0.0-20240606023456-4f3b6f3e1c2d
+    github.com/PhysicusPrime/golang-firmware-builder/packages v0.0.0-20240606023456-4f3b6f3e1c2d
+    github.com/PhysicusPrime/golang-firmware-builder/utils v0.0.0-20240606023456-4f3b6f3e1c2d
+)
--- packages/packages.go
+++ packages/packages.go
@@ -11,52 +11,23 @@ import (
 	"github.com/PhysicusPrime/golang-firmware-builder/utils"
 )
 
-// DownloadPackage lädt ein GNU-Paket herunter und entpackt es
-func DownloadPackage(pkg, dest string) string {
-	fmt.Printf("Downloading %s...\n", pkg)
-	utils.ProgressBar(pkg+" Download", 3) // <-- Korrigiert
-
-	url := fmt.Sprintf("https://ftp.gnu.org/gnu/%s/%s-latest.tar.gz", pkg, pkg)
-	tarball := filepath.Join(dest, pkg+".tar.gz")
-	if err := command.RunCommandLive("wget", "-O", tarball, url); err != nil {
-		log.Fatalf("Fehler beim Download von %s: %v", pkg, err)
-	}
-
-	srcDir := filepath.Join(dest, pkg)
-	if err := command.RunCommandLive("mkdir", "-p", srcDir); err != nil {
-		log.Fatalf("Fehler beim Erstellen von %s: %v", srcDir, err)
-	}
-	if err := command.RunCommandLive("tar", "xzf", tarball, "-C", srcDir, "--strip-components=1"); err != nil {
-		log.Fatalf("Fehler beim Entpacken von %s: %v", pkg, err)
-	}
-
-	fmt.Println(pkg, "heruntergeladen:", srcDir)
-	return srcDir
-}
-
-// BuildPackage konfiguriert, baut und installiert ein Paket ins RootFS
-func BuildPackage(srcDir, rootfs, cross string) {
-	if err := os.Chdir(srcDir); err != nil {
-		log.Fatalf("Fehler beim Wechseln ins Paketverzeichnis: %v", err)
-	}
-
-	if err := exec.Command("./configure", "--host="+cross, "--prefix="+rootfs).Run(); err != nil {
-		log.Fatalf("Fehler beim Konfigurieren von %s: %v", srcDir, err)
-	}
-	if err := exec.Command("make").Run(); err != nil {
-		log.Fatalf("Fehler beim Bauen von %s: %v", srcDir, err)
-	}
-	if err := exec.Command("make", "install").Run(); err != nil {
-		log.Fatalf("Fehler beim Installieren von %s: %v", srcDir, err)
-	}
-
-	log.Println("Package gebaut:", srcDir)
+// Map mit festen Versionen der Pakete
+var pkgVersions = map[string]string{
+	"bash":     "5.2",
+	"make":     "4.4",
+	"cmake":    "3.27.6",
+	"autoconf": "2.71",
+	"automake": "1.17.3",
+	"binutils": "2.41",
+	"libtool":  "2.4.7",
+	"fdisk":    "2.40.0",
+	"parted":   "3.5",
 }
 
 // DownloadOpkg lädt Opkg herunter und entpackt es
 func DownloadOpkg(dest string) string {
 	fmt.Println("Downloading Opkg...")
-	utils.ProgressBar("Opkg Download", 3) // <-- Korrigiert
+	utils.ProgressBar("Opkg Download", 3)
 
 	url := "https://downloads.openwrt.org/sources/opkg-0.6.1.tar.gz"
 	tarball := filepath.Join(dest, "opkg.tar.gz")
