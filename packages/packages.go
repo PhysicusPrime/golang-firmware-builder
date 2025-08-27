ec1ec8d77785dfe060315458f768999db7e7f116
Your Name <you@example.com>
2025-08-27 08:01:31 +0200

    Added

--- packages/packages.go
+++ packages/packages.go
@@ -11,7 +11,7 @@ import (
 	"github.com/PhysicusPrime/golang-firmware-builder/utils"
 )
 
-// Map mit festen Versionen der Pakete
+// Versions-Mapping für Pakete
 var pkgVersions = map[string]string{
 	"bash":     "5.2",
 	"make":     "4.4",
@@ -24,112 +24,7 @@ var pkgVersions = map[string]string{
 	"parted":   "3.5",
 }
 
-// BuildBashPackage baut Bash für Cross-Compile ins RootFS
-func BuildBashPackage(srcDir, rootfs, cross string) {
-	if err := os.Chdir(srcDir); err != nil {
-		log.Fatalf("Fehler beim Wechseln ins Bash-Verzeichnis: %v", err)
-	}
-
-	// Konfigurations-Flags für Cross-Compile
-	configureCmd := exec.Command(
-		"./configure",
-		"--host="+cross,
-		"--prefix="+rootfs,
-		"--disable-nls",         // keine Lokalisierung
-		"--without-bash-malloc", // alternative Speicherverwaltung
-		"--enable-static",       // statisch kompilieren
-		"--disable-shared",      // keine Shared Libraries
-	)
-	configureCmd.Env = append(os.Environ(), "CC="+cross+"gcc", "CFLAGS=-static", "LDFLAGS=-static")
-	configureCmd.Stdout = os.Stdout
-	configureCmd.Stderr = os.Stderr
-	if err := configureCmd.Run(); err != nil {
-		log.Fatalf("Fehler beim Konfigurieren von Bash: %v", err)
-	}
-
-	// make
-	makeCmd := exec.Command("make")
-	makeCmd.Stdout = os.Stdout
-	makeCmd.Stderr = os.Stderr
-	if err := makeCmd.Run(); err != nil {
-		log.Fatalf("Fehler beim Bauen von Bash: %v", err)
-	}
-
-	// make install
-	makeInstall := exec.Command("make", "install")
-	makeInstall.Stdout = os.Stdout
-	makeInstall.Stderr = os.Stderr
-	if err := makeInstall.Run(); err != nil {
-		log.Fatalf("Fehler beim Installieren von Bash: %v", err)
-	}
-
-	log.Println("Bash erfolgreich gebaut und ins RootFS installiert:", srcDir)
-}
-
-// DownloadOpkg lädt Opkg herunter und entpackt es
-func DownloadOpkg(dest string) string {
-	fmt.Println("Downloading Opkg...")
-	utils.ProgressBar("Opkg Download", 3)
-
-	url := "https://downloads.openwrt.org/sources/opkg-0.6.1.tar.gz"
-	tarball := filepath.Join(dest, "opkg.tar.gz")
-	if err := command.RunCommandLive("wget", "-O", tarball, url); err != nil {
-		log.Fatalf("Fehler beim Download von Opkg: %v", err)
-	}
-
-	srcDir := filepath.Join(dest, "opkg")
-	if err := command.RunCommandLive("mkdir", "-p", srcDir); err != nil {
-		log.Fatalf("Fehler beim Erstellen von %s: %v", srcDir, err)
-	}
-	if err := command.RunCommandLive("tar", "xzf", tarball, "-C", srcDir, "--strip-components=1"); err != nil {
-		log.Fatalf("Fehler beim Entpacken von Opkg: %v", err)
-	}
-
-	fmt.Println("Opkg heruntergeladen:", srcDir)
-	return srcDir
-}
-
-// BuildOpkg baut Opkg und installiert die Binärdatei ins RootFS
-func BuildOpkg(srcDir, rootfs, cross string) {
-	if err := os.Chdir(srcDir); err != nil {
-		log.Fatalf("Fehler beim Wechseln ins Opkg-Verzeichnis: %v", err)
-	}
-
-	cmd := exec.Command("make", fmt.Sprintf("CC=%sgcc", cross), fmt.Sprintf("PREFIX=%s", rootfs))
-	cmd.Env = append(os.Environ(), "CROSS_COMPILE="+cross)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Fehler beim Bauen von Opkg: %v", err)
-	}
-
-	binPath := filepath.Join(rootfs, "usr", "bin")
-	os.MkdirAll(binPath, 0755)
-	if err := exec.Command("cp", "opkg", binPath).Run(); err != nil {
-		log.Fatalf("Fehler beim Kopieren von Opkg: %v", err)
-	}
-
-	fmt.Println("Opkg installiert")
-}
-
-// SetupOpkgConf erstellt die Standardkonfiguration für Opkg im RootFS
-func SetupOpkgConf(rootfs string) {
-	etcDir := filepath.Join(rootfs, "etc", "opkg")
-	os.MkdirAll(etcDir, 0755)
-	conf := `dest root /
-lists_dir ext /var/lib/opkg
-option overlay_root /overlay
-option check_signature 0
-`
-	if err := os.WriteFile(filepath.Join(etcDir, "opkg.conf"), []byte(conf), 0644); err != nil {
-		log.Fatalf("Fehler beim Schreiben von opkg.conf: %v", err)
-	}
-
-	os.MkdirAll(filepath.Join(rootfs, "var/lib/opkg"), 0755)
-	os.MkdirAll(filepath.Join(rootfs, "overlay"), 0755)
-}
-
-// DownloadPackage lädt ein GNU-Paket herunter und entpackt es
+// ----------------- Allgemeine GNU-Pakete -----------------
 func DownloadPackage(pkg, dest string) string {
 	fmt.Printf("Downloading %s...\n", pkg)
 	utils.ProgressBar(pkg+" Download", 3)
@@ -157,12 +52,12 @@ func DownloadPackage(pkg, dest string) string {
 	return srcDir
 }
 
-// BuildPackage konfiguriert, baut und installiert ein Paket ins RootFS
 func BuildPackage(srcDir, rootfs, cross string) {
 	if err := os.Chdir(srcDir); err != nil {
 		log.Fatalf("Fehler beim Wechseln ins Paketverzeichnis: %v", err)
 	}
 
+	// Standard configure -> make -> make install
 	if err := exec.Command("./configure", "--host="+cross, "--prefix="+rootfs).Run(); err != nil {
 		log.Fatalf("Fehler beim Konfigurieren von %s: %v", srcDir, err)
 	}
@@ -174,4 +69,95 @@ func BuildPackage(srcDir, rootfs, cross string) {
 	}
 
 	log.Println("Package gebaut:", srcDir)
+}
+
+// ----------------- Bash spezielles Build (Cross-Compile) -----------------
+func BuildBashPackage(srcDir, rootfs, cross string) {
+	if err := os.Chdir(srcDir); err != nil {
+		log.Fatalf("Fehler beim Wechseln ins Bash-Verzeichnis: %v", err)
+	}
+
+	cmd := exec.Command("./configure",
+		"--host="+cross,
+		"--prefix="+rootfs,
+		"--disable-nls",
+		"--without-bash-malloc")
+	cmd.Env = append(os.Environ(), "CC="+cross+"gcc")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Fehler beim Konfigurieren von Bash: %v", err)
+	}
+
+	if err := exec.Command("make").Run(); err != nil {
+		log.Fatalf("Fehler beim Bauen von Bash: %v", err)
+	}
+	if err := exec.Command("make", "install").Run(); err != nil {
+		log.Fatalf("Fehler beim Installieren von Bash: %v", err)
+	}
+
+	log.Println("Bash installiert:", srcDir)
+}
+
+// ----------------- Opkg -----------------
+func DownloadOpkg(dest string) string {
+	fmt.Println("Downloading Opkg...")
+	utils.ProgressBar("Opkg Download", 3)
+
+	url := "https://downloads.openwrt.org/sources/opkg-0.6.1.tar.gz"
+	tarball := filepath.Join(dest, "opkg.tar.gz")
+	if err := command.RunCommandLive("wget", "-O", tarball, url); err != nil {
+		log.Fatalf("Fehler beim Download von Opkg: %v", err)
+	}
+
+	srcDir := filepath.Join(dest, "opkg")
+	if err := command.RunCommandLive("mkdir", "-p", srcDir); err != nil {
+		log.Fatalf("Fehler beim Erstellen von %s: %v", srcDir, err)
+	}
+	if err := command.RunCommandLive("tar", "xzf", tarball, "-C", srcDir, "--strip-components=1"); err != nil {
+		log.Fatalf("Fehler beim Entpacken von Opkg: %v", err)
+	}
+
+	fmt.Println("Opkg heruntergeladen:", srcDir)
+	return srcDir
+}
+
+func BuildOpkg(srcDir, rootfs, cross string) {
+	if err := os.Chdir(srcDir); err != nil {
+		log.Fatalf("Fehler beim Wechseln ins Opkg-Verzeichnis: %v", err)
+	}
+
+	cmd := exec.Command("make",
+		fmt.Sprintf("CC=%sgcc", cross),
+		fmt.Sprintf("PREFIX=%s", rootfs))
+	cmd.Env = append(os.Environ(), "CROSS_COMPILE="+cross)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Fehler beim Bauen von Opkg: %v", err)
+	}
+
+	binPath := filepath.Join(rootfs, "usr", "bin")
+	os.MkdirAll(binPath, 0755)
+	if err := exec.Command("cp", "opkg", binPath).Run(); err != nil {
+		log.Fatalf("Fehler beim Kopieren von Opkg: %v", err)
+	}
+
+	fmt.Println("Opkg installiert")
+}
+
+func SetupOpkgConf(rootfs string) {
+	etcDir := filepath.Join(rootfs, "etc", "opkg")
+	os.MkdirAll(etcDir, 0755)
+	conf := `dest root /
+lists_dir ext /var/lib/opkg
+option overlay_root /overlay
+option check_signature 0
+`
+	if err := os.WriteFile(filepath.Join(etcDir, "opkg.conf"), []byte(conf), 0644); err != nil {
+		log.Fatalf("Fehler beim Schreiben von opkg.conf: %v", err)
+	}
+
+	os.MkdirAll(filepath.Join(rootfs, "var/lib/opkg"), 0755)
+	os.MkdirAll(filepath.Join(rootfs, "overlay"), 0755)
 }
